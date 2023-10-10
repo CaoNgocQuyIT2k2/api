d2097dfe482178fdeda6b8dcfb8aa2a49cf8d69f
CaoNgocQuyIT2k2 <caongocquyit2k2@gmail.com>
2023-10-10 18:34:31 +0700

    count cart

--- cyberPhone/js/cart_index.js
+++ cyberPhone/js/cart_index.js
@@ -1,101 +1,52 @@
-var dssp = [];
+// Biến đếm cho ID của bảng
+let tableIdCounter = 0;
+window.tableIdCounter = tableIdCounter;
+export { tableIdCounter };
 
-// Khi trang được tải lại, hiển thị spinner
-document.addEventListener("DOMContentLoaded", () => {
-  showSpinner();
-  fetchFoodList(); // Gọi hàm để tải danh sách sản phẩm lại sau khi trang được tải lại
-});
 
-//selected Id của product chọn để edit
-var selectedId = null;
 
-function renderCart(productArr) {
-  // Xóa dữ liệu hiện tại trong mảng dssp
-  dssp.length = 0;
+import productList from './index.js';
 
+
+export function updateCartCount() {
+  const totalQuantity = productList.reduce((total, item) => total + item.quality, 0);
+  const cartCountElement = document.getElementById("cart-count");
+  cartCountElement.textContent = totalQuantity;
+}
+
+window.updateCartCount = updateCartCount;
+
+
+export function renderItemList(productList) {
   var contentHTML = "";
-  for (var i = 0; i < productArr.length; i++) {
-    var product = productArr[i];
-    // Thêm sản phẩm vào mảng dssp
-    dssp.push(product);
-    var trString = `<tr>
-    <td>${cartItem.name}</td>
-    <td>${cartItem.price}</td>
-    <td>${cartItem.img}</td>
-    <td>${cartItem.type}</td>
-    <td>${cartItem.quantity}</td>
-              <td>
-                <button onclick="addItem(${product.id})" class="btn btn-danger">+</button>
-                <button onclick="subItem(${product.id})" class="btn btn-warning  ">-</button>
-                <button onclick="deleteItem(${product.id})" class="btn btn-warning  ">X</button>
-              </td>
-          </tr>`;
+  for (var i = 0; i < productList.length; i++) {
+    var item = productList[i];
+
+    // Tạo ID duy nhất cho bảng
+    var tableId = `itemTable${tableIdCounter}`;
+    tableIdCounter++; // Tăng biến đếm sau khi sử dụng
+    updateCartCount()
+    var trString = `<tr id="${tableId}">
+        <td>${tableIdCounter}</td>
+        <td>${item.name}</td>
+        <td>${item.price}</td>
+        <td>${item.img}</td>
+        <td>${item.type}</td>
+        <td>${item.quality}</td>
+        <td>
+            <button onclick="addItem('${item.name}', '${tableId}')" class="btn btn-success">+</button>
+            <button onclick="subItem('${item.name}', '${tableId}')" class="btn btn-danger pr-3">-</button>
+        </td>
+    </tr>`;
     contentHTML += trString;
+
   }
-  document.getElementById("tblDanhSachSP").innerHTML = contentHTML;
+
+  // Kiểm tra xem phần tử "tblGioHang" có tồn tại hay không trước khi thay đổi nội dung
+  var tblGioHang = document.getElementById("tblGioHang");
+  if (tblGioHang) {
+    tblGioHang.innerHTML = contentHTML;
+  }
 }
 
-  
-  
 
-// gọi api lấy danh sách sản phẩm trong giỏ hàng đang có từ sever
-axios({
-  url: "https://6522cecef43b17938414ed9a.mockapi.io/Cart",
-  method: "GET",
-})
-  .then((res) => {
-    //api trả về thành công
-    renderProductionList(res.data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-let productList = []; // Khai báo productList ở phạm vi toàn cục
-
-// Hàm gọi API và lấy danh sách sản phẩm
-function getProducts() {
-  axios({
-    url: "https://6522cecef43b17938414ed9a.mockapi.io/Cart",
-    method: "GET",
-  })
-    .then((res) => {
-      // API trả về thành công
-      productList = res.data; // Gán danh sách sản phẩm cho biến productList
-
-      // Gọi hàm filterProducts để hiển thị danh sách sản phẩm ban đầu
-    //   filterProducts();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-
-//------------------------------Lọc sản phẩm--------------------
-
-// // Hàm lọc danh sách sản phẩm theo loại sản phẩm
-// function filterProducts() {
-//   var filterSelect = document.getElementById("filterSelect");
-//   var selectedOption = filterSelect.value;
-
-//   if (selectedOption !== "") {
-//     var filteredProducts = productList.filter(function (product) {
-//       return product.type.toLowerCase() === selectedOption;
-//     });
-
-//     renderProductionList(filteredProducts);
-//   } else {
-//     renderProductionList(productList);
-//   }
-// }
-
-// // Khi sự kiện "change" xảy ra trên dropdown, gọi hàm filterProducts
-// var filterSelect = document.getElementById("filterSelect");
-// filterSelect.addEventListener("change", function () {
-//   filterProducts();
-// });
-
-// // Gọi hàm getProducts để lấy danh sách sản phẩm từ API
-// getProducts();
-
-//---------------------Giỏ hàng----------------------------
--- cyberPhone/js/index.js
+++ cyberPhone/js/index.js
@@ -1,21 +1,26 @@
-// Button shop 
+// Button shop
 function scrollToProducts() {
-  // Lấy phần tử "products" bằng cách sử dụng id hoặc class
+  // Lấy phần tử "products" bằng cách sử dụng class
   var productsElement = document.querySelector(".products");
 
   // Sử dụng phương thức scrollIntoView để cuộn trang đến phần tử "products"
-  productsElement.scrollIntoView({ behavior: "smooth" });
+  if (productsElement) {
+    productsElement.scrollIntoView({ behavior: "smooth" });
+  }
 }
+window.scrollToProducts = scrollToProducts
+window.filterProducts = filterProducts
 var dssp = []; // Định nghĩa biến dssp ở ngoài phạm vi của hàm
 
 function renderProductionList(productArr) {
-    var productContainer = document.getElementById('product-container');
-    productContainer.innerHTML = ''; // Xóa nội dung cũ trong container trước khi thêm dữ liệu mới
-  
-    productArr.forEach(product => {
-      var productDiv = document.createElement('div');
-      productDiv.classList.add('item');
-      productDiv.innerHTML = `
+  var productContainer = document.getElementById("product-container");
+  productContainer.innerHTML = ""; // Xóa nội dung cũ trong container trước khi thêm dữ liệu mới
+
+  productArr.forEach((product) => {
+    var productDiv = document.createElement("div");
+    productDiv.classList.add("item");
+    productDiv.innerHTML = `
+      <div class="item">
         <img class="image" src="${product.img}" alt="${product.name}" />
         <div class="desc">
           <div class="detail">
@@ -31,75 +36,202 @@ function renderProductionList(productArr) {
           </div>
           <div class="price">
             <span>${product.price}$</span>
-            <!-- Thêm nút "Add to Cart" vào sản phẩm -->
-<button class="add-to-cart" onclick="addToCart">Add to Cart</button>
-
+            <button class="add-to-cart">Add to Cart</button>
           </div>
         </div>
-      `;
-      productContainer.appendChild(productDiv);
+      </div>
+    `;
+
+    productContainer.appendChild(productDiv);
+
+    // Thêm sự kiện click vào nút "Add to Cart"
+    const addToCartButton = productDiv.querySelector(".add-to-cart");
+    addToCartButton.addEventListener("click", () => {
+      addToCart(productDiv);
     });
+  });
 }
-
-// gọi api lấy danh sách sản phẩm đang có từ sever 
-axios({
-  url: "https://6520dbe6906e276284c4beec.mockapi.io/Products",
-  method: "GET",
-})
-.then((res) => {
-  //api trả về thành công
-  dssp = res.data; // Gán danh sách sản phẩm cho biến dssp
-
-  // Gọi hàm filterProducts để hiển thị danh sách sản phẩm ban đầu
-  filterProducts();
-
-})
-.catch((err) => {
-  console.log(err);
-});
-
-
-
-
-
-
-
-
 
 
 //------------------------------Lọc sản phẩm--------------------
 // Hàm lọc danh sách sản phẩm theo loại sản phẩm
 function filterProducts() {
   var filterSelect = document.getElementById("filterSelect");
-  var selectedOption = filterSelect.value;
 
-  // Kiểm tra nếu người dùng đã chọn một loại sản phẩm
-  if (selectedOption !== "") {
+  // Kiểm tra xem filterSelect có tồn tại không
+  if (filterSelect) {
+    var selectedOption = filterSelect.value;
+
     // Lọc danh sách sản phẩm dựa trên loại sản phẩm đã chọn
-    var filteredProducts = dssp.filter(function (product) {
-      return product.type.toLowerCase() === selectedOption;
-    });
+    var filteredProducts;
+
+    if (selectedOption === "") {
+      // Nếu chọn "Tất cả", trả về tất cả sản phẩm
+      filteredProducts = dssp;
+    } else {
+      // Ngược lại, lọc theo loại sản phẩm đã chọn
+      filteredProducts = dssp.filter(function (product) {
+        return product.type.toLowerCase() === selectedOption;
+      });
+    }
 
     // Gọi hàm để hiển thị danh sách sản phẩm đã được lọc
     renderProductionList(filteredProducts);
-  } else {
-    // Nếu người dùng chọn "Tất cả", hiển thị danh sách sản phẩm ban đầu
-    renderProductionList(dssp);
   }
 }
 
+
+// Gọi api lấy danh sách sản phẩm đang có từ server
+axios({
+  url: "https://6520dbe6906e276284c4beec.mockapi.io/Products",
+  method: "GET",
+})
+  .then((res) => {
+    // Api trả về thành công
+    dssp = res.data; // Gán danh sách sản phẩm cho biến dssp
+
+    // Gọi hàm filterProducts để hiển thị danh sách sản phẩm ban đầu
+    filterProducts();
+  })
+  .catch((err) => {
+    console.log(err);
+  }); 
 //------------------------Giỏ hàng--------------------------
 
-import { getDataItem } from "./cart_controller.js";
-const cart = []; // Định nghĩa biến cart trong cart_controller.js
+// Khai báo mảng để lưu trữ các sản phẩm trong giỏ hàng
+const cart = [];
 
 
-function addToCart(productElement) {
-  const selectedProduct = getDataItem(productElement);
-  productList.push(selectedProduct); // Thêm sản phẩm vào mảng productList
-  console.log("🚀 ~ productList:", productList)
-  // Cập nhật giao diện giỏ hàng và thực hiện các công việc khác nếu cần
+// Hàm để lấy thông tin sản phẩm từ phần tử sản phẩm
+function getDataItem(productElement) {
+  // Lấy các phần tử con tương ứng trong phần tử sản phẩm
+  var nameElement = productElement.querySelector("h3");
+  var priceElement = productElement.querySelector(".price span");
+  var imgElement = productElement.querySelector(".image");
+  var typeElement = productElement.querySelector("p");
+
+  // Trích xuất thông tin từ các phần tử HTML
+  var nameItem = nameElement.textContent;
+  var priceItem = priceElement.textContent;
+  var imgItem = imgElement.src;
+  var typeItem = typeElement.textContent;
+
+  // Tạo đối tượng sản phẩm từ thông tin trích xuất
+  return {
+    name: nameItem,
+    price: priceItem,
+    img: imgItem,
+    type: typeItem,
+  };
 }
 
-// Hãy chắc chắn rằng bạn đã import hàm getDataItem từ file cart_controller.js ở file index.js.
+ let productList = [];
+
+let productList_localStorage = "productList_localStorage";
+
+// Render lại data từ local storage khi user reload 
+var dataJson = localStorage.getItem(productList_localStorage);
+
+  //
+  import { tableIdCounter } from "./cart_index.js";
+  // Import và sử dụng hàm renderItemList
+  import { renderItemList } from "./cart_index.js";
+
+  // Sử dụng hàm renderItemList ở đây
+  if (dataJson != null) {
+    // Kiểm tra dữ liệu dưới local storage có tồn tại hay không trước khi render
+    productList = JSON.parse(dataJson); // Gán dữ liệu từ localStorage vào productList
+    // Export biến productList
+    // renderItemList(productList);
+  }
+
+// Export biến productList
+export default productList;
+console.log("🚀 ~ productList:", productList)
+window.productList = productList;
+
+
+function calculateTotalQuality() {
+  const totalQuality = productList.reduce((total, item) => total + item.quality, 0);
+  return totalQuality;
+}
+
+// Gọi hàm để tính tổng quality
+const totalQuality = calculateTotalQuality();
+
+// Hiển thị giá trị tổng quality trong thẻ cart-count
+const cartCountElement = document.getElementById("cart-count");
+cartCountElement.textContent = totalQuality;
+
+
+
+function calculateTotalQuantityInCart() {
+  const totalQuantity = cart.reduce((total, item) => total + item.quality, 0);
+  console.log("🚀 ~ totalQuantity:", totalQuantity)
+  return totalQuantity;
+}
+
+import { updateCartCount } from "./cart_index.js";
+
+
+// Hàm để thêm sản phẩm vào mảng productList
+function addToProductList(product) {
+  // Kiểm tra xem sản phẩm đã có trong danh sách sản phẩm chưa
+  const existingProductIndex = productList.findIndex(
+    (item) => item.name === product.name
+  );
+
+  if (existingProductIndex !== -1) {
+    // Nếu sản phẩm đã tồn tại trong danh sách, tăng quality lên 1 đơn vị
+    productList[existingProductIndex].quality += 1;
+  } else {
+    // Nếu sản phẩm chưa tồn tại trong danh sách, thêm vào với quality là 1
+    product.quality = 1;
+    productList.push(product);
+  }
+
+  // Log thông báo vào console
+  console.log("Sản phẩm đã được thêm vào danh sách sản phẩm:", product.name);
+
+  // Log danh sách sản phẩm vào console
+  console.log("Danh sách sản phẩm:", productList);
+
+  // Convert array to json để lưu xuống local storage
+  var dataJson = JSON.stringify(productList); // Sửa thành productList
+  // Lưu json vào local storage
+  localStorage.setItem(productList_localStorage, dataJson);
+  // Lưu số lượng sản phẩm trong giỏ hàng vào local storage
+  const totalQuantityInCart = calculateTotalQuantityInCart();
+  localStorage.setItem("totalQuantityInCart", totalQuantityInCart);
+
+}
+
+
+
+// Hàm để thêm sản phẩm vào giỏ hàng
+function addToCart(productElement) {
+  // Lấy thông tin sản phẩm từ hàm getDataItem
+  const selectedProduct = getDataItem(productElement);
+  updateCartCount();
+  // Kiểm tra xem sản phẩm đã có trong giỏ hàng chưa
+  const existingCartItem = cart.find(
+    (item) => item.name === selectedProduct.name
+  );
+
+  if (existingCartItem) {
+    // Nếu sản phẩm đã có trong giỏ hàng, tăng quality lên 1 đơn vị
+    existingCartItem.quality += 1;
+  } else {
+    // Nếu sản phẩm chưa có trong giỏ hàng, thêm sản phẩm vào giỏ hàng với quality là 1
+    selectedProduct.quality = 1;
+    cart.push(selectedProduct);
+  }
+
+  // // Cập nhật số lượng sản phẩm trong thẻ <span id="cart-count">
+  // const cartCountElement = document.getElementById("cart-count");
+  // cartCountElement.textContent = calculateTotalQuantityInCart();
+
+  // Gọi hàm để thêm sản phẩm vào danh sách sản phẩm
+  addToProductList(selectedProduct);
+}
 
